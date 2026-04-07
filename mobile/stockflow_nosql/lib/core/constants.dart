9290abd87c2bb07d506494119411f7b27a2591e7
paulocastelo <oluap.castelo@gmail.com>
2026-04-06 23:18:45 -0300

    mobile: use platform-aware api base url

--- mobile/stockflow_nosql/lib/core/constants.dart
+++ mobile/stockflow_nosql/lib/core/constants.dart
@@ -1,11 +1,23 @@
+import 'package:flutter/foundation.dart';
+
 class AppConstants {
   const AppConstants._();
 
   // Em celular físico, use o IP da máquina na rede local.
   // Não inclua `/api` aqui, porque os endpoints já adicionam esse prefixo.
   // No emulador Android, use `http://10.0.2.2:<porta>`.
-  static const String apiBaseUrl = 'http://10.0.2.2:8080';
-  static const String baseUrl = apiBaseUrl;
+  static String get apiBaseUrl {
+    if (kIsWeb) {
+      return 'http://localhost:8080';
+    }
+
+    return switch (defaultTargetPlatform) {
+      TargetPlatform.android => 'http://10.0.2.2:8080',
+      _ => 'http://localhost:8080',
+    };
+  }
+
+  static String get baseUrl => apiBaseUrl;
 
   static const String tokenKey = 'stockflow.token';
   static const String userKey = 'stockflow.user';
